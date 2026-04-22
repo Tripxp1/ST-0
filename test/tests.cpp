c3b48a487b5627e95bcd6bcf50ba1ac10c8efa08
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-04-22 10:57:12 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,11 +4,26 @@
 #include "alg.h"
 
 TEST(algtest, test1) {
-  uint64_t result = factorial(1);
+  uint64_t result = factorial(0);
   EXPECT_EQ(1, result);
 }
 
 TEST(algtest, test2) {
+  uint64_t result = factorial(1);
+  EXPECT_EQ(1, result);
+}
+
+TEST(algtest, test3) {
   uint64_t result = factorial(2);
   EXPECT_EQ(2, result);
+}
+
+TEST(algtest, test4) {
+  uint64_t result = factorial(5);
+  EXPECT_EQ(120, result);
+}
+
+TEST(algtest, test5) {
+  uint64_t result = factorial(10);
+  EXPECT_EQ(3628800, result);
 }
